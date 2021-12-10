2768830e2c8496d500b941f5877eef08c1f74d00
Christopher Neal <christopher.m.neal@gmail.com>
2021-12-10 08:56:08 -0800

    pass third test for Text.anagram_checker(), returns 'anagram!'

--- lib/anagrams_and_antigrams.rb
+++ lib/anagrams_and_antigrams.rb
@@ -5,7 +5,7 @@ class Text
     @input1 = input1
   end
   def anagram_checker(input2)
-    if @input1.split("").sort() == input2.split("").sort()
+    if @input1.downcase().split("").sort() == input2.downcase().split("").sort()
       "anagram!"
     else
       "not an anagram"
