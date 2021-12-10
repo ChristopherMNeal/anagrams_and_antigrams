2e14334a774eddb9131d674bcdf17ed3275f1817
Christopher Neal <christopher.m.neal@gmail.com>
2021-12-10 14:15:17 -0800

    add scripts

--- ruby_script.rb
+++ ruby_script.rb
@@ -1,3 +1,17 @@
 #!/usr/bin/ruby
 require ('./lib/anagrams_and_antigrams.rb')
 
+puts "\n\n\n\n\nWELXCOME TO THE ANAGRAM CHECKER \ntype 'quit' at any time to exit\n\n"
+input1 = ""
+input2 = ""
+
+while (input1 != 'quit' && input2 != 'quit')
+  puts "\nenter the first word or phrase you'd like to check"
+  input1 = gets.chomp
+  text = Text.new(input1)
+  if input1 != 'quit'
+    puts "\nenter the second word or phrase you'd like to check"
+    input2 = gets.chomp
+    puts "\n\n" + text.anagram_checker(input2) + "\n\n"
+  end
+end
