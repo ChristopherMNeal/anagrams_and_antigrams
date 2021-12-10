65e4c001e28b0efa3850b210d96668371cc80056
Christopher Neal <christopher.m.neal@gmail.com>
2021-12-10 11:37:23 -0800

    refactor Text.word_checker?() and to split and loop through multiple words, pass all tests

--- lib/anagrams_and_antigrams.rb
+++ lib/anagrams_and_antigrams.rb
@@ -2,10 +2,13 @@ require("pry")
 
 module Utility_Functions
   def word_checker?(input)
-    if (input.downcase.count 'aeiou') >= 1
-      true
-    else
-      false
+    words = input.split(" ")
+    words.each do |word|
+      if (word.downcase.count 'aeiou') >= 1
+        return true
+      else
+        return false
+      end
     end
   end
 
@@ -17,10 +20,6 @@ module Utility_Functions
   def array_intersection(input1, input2)
     input1 & input2
   end
-
-  def sentence_splitter(sentence)
-    sentence.split(" ")
-  end
 end
 
 class Text
@@ -31,22 +30,14 @@ class Text
   end
 
   def anagram_checker(input2)
-    sentence_array1 = sentence_splitter(@input1)
-    sentence_array2 = sentence_splitter(input2)
     split1 = word_splitter(@input1)
     split2 = word_splitter(input2)
     compared_array = array_intersection(split1, split2)
-    sentence_array1.each do |word|
-      if word_checker?(word) == false
-        "'#{@input1}' is not an real word"
-      end
-    end
-    sentence_array2.each do |word|
-      if word_checker?(word) == false
-        "'#{input2}' is not a real word"
-      end
-    end
-    if (compared_array.length() == split1.length) && (compared_array.length() == split2.length)
+    if word_checker?(@input1) == false
+      "'#{@input1}' is not an real word"
+    elsif word_checker?(input2) == false
+      "'#{input2}' is not a real word"
+    elsif (compared_array.length() == split1.length) && (compared_array.length() == split2.length)
       "anagram!"
     elsif (compared_array.length() == 0)
       "antigram!"
