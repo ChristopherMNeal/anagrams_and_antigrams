eada92419e68cf23a005684ac7190e78057c02e4
Christopher Neal <christopher.m.neal@gmail.com>
2021-12-10 11:26:03 -0800

    WIP: add first test for Text.sentence_splitter()

--- lib/anagrams_and_antigrams.rb
+++ lib/anagrams_and_antigrams.rb
@@ -10,11 +10,16 @@ module Utility_Functions
   end
 
   def word_splitter(input)
-    input.downcase().split('').sort() 
+    input.downcase().split('').sort()
+    # .gsub(/[^a-z0-9]/,'')
   end
 
   def array_intersection(input1, input2)
     input1 & input2
+  end
+
+  def sentence_splitter(sentence)
+    sentence.split(" ")
   end
 end
 
@@ -26,21 +31,26 @@ class Text
   end
 
   def anagram_checker(input2)
+    sentence_array1 = sentence_splitter(@input1)
+    sentence_array2 = sentence_splitter(input2)
     split1 = word_splitter(@input1)
     split2 = word_splitter(input2)
     compared_array = array_intersection(split1, split2)
-    if word_checker?(@input1) == false
-      "'#{@input1}' is not an real word"
-    elsif word_checker?(input2) == false
-      "'#{input2}' is not a real word"
-    # elsif word_splitter(@input1) == word_splitter(input2)
-      # "anagram!"
-    elsif (compared_array.length() == split1.length) && (compared_array.length() == split2.length)
+    sentence_array1.each do |word|
+      if word_checker?(word) == false
+        "'#{@input1}' is not an real word"
+      end
+    end
+    sentence_array2.each do |word|
+      if word_checker?(word) == false
+        "'#{input2}' is not a real word"
+      end
+    end
+    if (compared_array.length() == split1.length) && (compared_array.length() == split2.length)
       "anagram!"
     elsif (compared_array.length() == 0)
       "antigram!"
     else
-      # "not an anagram"
       "neither an anagram nor an antigram! the two inputs have letters '#{compared_array.join(", ")}' in common"
     end
   end
