9f8c3324d1a133b310611b2feb1431c217744e57
Christopher Neal <christopher.m.neal@gmail.com>
2021-12-10 10:54:19 -0800

    add first test for Text.array_intersection()

--- lib/anagrams_and_antigrams.rb
+++ lib/anagrams_and_antigrams.rb
@@ -8,8 +8,13 @@ module Utility_Functions
       false
     end
   end
+
   def word_splitter(input)
     input.downcase().split('').sort() 
+  end
+
+  def array_intersection(input1, input2)
+    # []
   end
 end
 
@@ -19,15 +24,24 @@ class Text
   def initialize(input1)
     @input1 = input1
   end
+
   def anagram_checker(input2)
+    split1 = word_splitter(@input1)
+    split2 = word_splitter(input2)
+    # compared_array = array_intersection(split1, split2)
     if word_checker?(@input1) == false
       "'#{@input1}' is not an real word"
     elsif word_checker?(input2) == false
       "'#{input2}' is not a real word"
     elsif word_splitter(@input1) == word_splitter(input2)
       "anagram!"
+    # elsif (compared_array.length() == split1.length) && (compared_array.length() == split2.length)
+    #   "anagram!"
+    # elsif (compared_array.length() == 0)
+    #   "antigram!"
     else
       "not an anagram"
+      # "neither an anagram nor an antigram! the two words have letters '#{compared_array.join(", ")}' in common"
     end
   end
 end
