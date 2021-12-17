a7f138b156a8f5f3df793d4313215af005d03bec
Christopher Neal <christopher.m.neal@gmail.com>
2021-12-16 18:46:42 -0800

    refactor to return instance variables to better separate UI

--- lib/anagrams_and_antigrams.rb
+++ lib/anagrams_and_antigrams.rb
@@ -2,6 +2,10 @@ require("pry")
 require ("dictionary_lookup")
 
 module Utility_Functions
+  
+  # attr_accessor
+  # end
+
   def word_checker(input, dictionary)
     words = input.gsub(/[^a-zA-Z ]/,'').split(" ")
     not_word = []
@@ -34,25 +38,43 @@ end
 
 class Text
   include Utility_Functions
+
+  attr_reader(:input1, :input2, :result, :fake_words, :letters_in_common, :number_of_letters)
   
   def initialize(input1)
     @input1 = input1
+    @input2 = ""
+    @result = ""
+    @fake_words = ""
+    @letters_in_common = ""
+    @number_of_letters = 0
+
   end
 
   def anagram_checker(input2, dictionary)
+    @input2 = input2
     split1 = word_splitter(@input1)
     split2 = word_splitter(input2)
     compared_array = array_intersection(split1, split2)
     if word_checker(@input1, dictionary).length >= 1
-      "'error! the following aren't real words: #{word_checker(@input1, dictionary).join(", ")}'"
+      @fake_words = word_checker(@input1, dictionary).join(", ")
+      @result = "error"
+      # "'error! the following aren't real words: #{word_checker(@input1, dictionary).join(", ")}'"
     elsif word_checker(input2, dictionary).length >= 1
-      "error! the following aren't real words: '#{word_checker(input2, dictionary).join(", ")}'"
+      @fake_words = word_checker(input2, dictionary).join(", ")
+      @result = "error"
+      # "error! the following aren't real words: '#{word_checker(input2, dictionary).join(", ")}'"
     elsif split1 == split2
-      "'#{@input1}' and '#{input2}' are anagrams!"
+      @result = "anagrams"
+      # "'#{@input1}' and '#{input2}' are anagrams!"
     elsif (compared_array.length() == 0)
-      "'#{@input1}' and '#{input2}' are antigrams!"
+      @result = "antigrams"
+      # "'#{@input1}' and '#{input2}' are antigrams!"
     else
-      "'#{@input1}' and '#{input2}' are neither anagrams nor a antigrams! they have #{compared_array.length} letters in common: '#{compared_array.join(", ")}'"
+      @letters_in_common = compared_array.join(", ")
+      @number_of_letters = compared_array.length
+      @result = "neither anagrams nor antigrams"
+      # "'#{@input1}' and '#{input2}' are neither anagrams nor a antigrams! they have #{compared_array.length} letters in common: '#{compared_array.join(", ")}'"
     end
   end
 end
